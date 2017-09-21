7732bfc155912b89d3b2d2520aef61d99226738e
851258786 <zf851258786@163.com>
2017-09-21 11:11:49 +0800

    Update calibrate.c

--- init/calibrate.c
+++ init/calibrate.c
@@ -6,10 +6,6 @@
 
 #include <linux/jiffies.h>
 #include <linux/delay.h>
-#include <linux/init.h>
-#include <linux/timex.h>
-#include <linux/smp.h>
-#include <linux/percpu.h>
 
 unsigned long lpj_fine;
 unsigned long preset_lpj;
